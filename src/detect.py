0464ec67fd2bae277aa66fb349b053c5c923a63f
Hare00 <hareplant00@gmail.com>
2024-08-22 16:50:26 +0900

    v1.0.1 - output txt file

--- src/detect.py
+++ src/detect.py
@@ -1,18 +1,23 @@
 from ultralytics import YOLO
+import os
 
-name = "3198.jpg"
+folder_path = '../datasets/test/images/'
+file_names = os.listdir(folder_path)
+file_pathes = [folder_path+f for f in file_names]
 
 model = YOLO('runs/detect/train3/weights/best.pt')
-results = model([f"../datasets/test/images/{name}"])
+results = model(file_pathes)
 class_names = model.names
 
-for result in results:
-    boxes = result.boxes
-    print("\n結果 : ")
-    for box in boxes:
-        class_id = int(box.cls[0])
-        confidence = box.conf[0]
-        class_name = class_names[class_id]
-        print(f"Class: {class_name}, Confidence: {confidence:.3f}")
-        
-    result.save(filename=f"res/{name}")  # save to disk
+with open("res/res.txt","w") as o:
+    for result,name in zip(results,file_names):
+        boxes = result.boxes
+        print(f"# result : {name}",file=o)
+        for box in boxes:
+            class_id = int(box.cls[0])
+            confidence = box.conf[0]
+            class_name = class_names[class_id]
+            print(f"Class: {class_name}, Confidence: {confidence:.3f}",file=o)
+        print('\n',file=o)
+            
+        result.save(filename=f"res/r{name}")  # save to disk
