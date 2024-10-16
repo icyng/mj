5fdfbd274d7db70ff0340398175a04be200b7b21
Hare00 <hareplant00@gmail.com>
2024-10-16 12:38:57 +0900

    training

--- src/train.py
+++ src/train.py
@@ -8,9 +8,8 @@ results = model.train(
     data='../datasets/coco128.yml', 
     epochs=100, 
     batch=5,
-    device='mps',
-    device=[0,1]
+    # device='mps',
+    # device=[0,1]
 )
 
-# Evaluate the model's performance on the validation set
 results = model.val()
