d6b841ac3a833f155756d52b36f7e960dbc66039
icyng <take523oliver@gmail.com>
2025-07-15 14:14:53 +0900

    feature: show image

--- mj/models/tehai/myyolo.py
+++ mj/models/tehai/myyolo.py
@@ -1,30 +1,16 @@
 from ultralytics import YOLO
 
-# 字牌と赤牌を2文字表記に揃える置換ルール
-replace = {
-    '5mr': '0m',
-    '5pr': '0p',
-    '5sr': '0s',
-    'hak': 'hk',
-    'hat': 'ht',
-    'nan': 'na',
-    'pei': 'pe',
-    'sha': 'sh',
-    'ton': 'to',
-    'tyn': 'ty',
-}
-
 def MYYOLO(
     model_path: str, 
-    file_path: str,
-    conf: float = 0.4,
-    iou: float = 0.4
+    image_path: str,
+    conf: float = 0.5,
+    iou: float = 0.5
 ):
     """指定されたモデルを使用して指定画像内の牌を検出し、結果を整形して返す
 
     Args:
         model_path (str): モデルのパス
-        file_path (str): ファイルのパス
+        image_path (str): ファイルのパス
         conf (float, optional): 検出の信頼度閾値
         iou (float, optional): IoUの閾値
     Returns:
@@ -34,8 +20,14 @@ def MYYOLO(
     """
     
     model = YOLO(model_path)
-    result = model(file_path, conf=conf, iou=iou)[0]
-    cls_names = {k: replace.get(v, v) for k, v in model.names.items()}
+    result = model.predict(
+        source=image_path,
+        conf=conf,
+        iou=iou,
+        save=False
+    )[0]
+    result.show(font_size=3, line_width=2)
+    cls_names = model.names
     
     boxes = result.boxes
     tile_infos = []
@@ -52,4 +44,5 @@ def MYYOLO(
     
     tile_infos.sort(key=lambda x:x['point'])
     tile_names = [h['class'] for h in tile_infos]
+
     return tile_infos, tile_names
